2ec543f5380327bbc3ed26407619762c77c8a686
wahyu12v <mkzcmd@.com>
2024-05-10 00:04:07 +0700

    login front end

--- resources/views/v_login.blade.php
+++ resources/views/v_login.blade.php
@@ -17,61 +17,35 @@
     <link rel="stylesheet" href="{{asset ('AdminLTE/')}}/dist/css/adminlte.min.css">
 </head>
 
-<body class="hold-transition login-page">
-    <div class="login-box">
-        <!-- /.login-logo -->
-        <div class="card card-outline card-primary">
-            <div class="card-header text-center">
-                <a href="{{asset ('AdminLTE/')}}/index2.html" class="h1"><b>PERPUSTAKAAN SD 01 MINAS TIMUR</a>
+<body class="hold-transition login-page" style="background-image: url('https://img.freepik.com/free-vector/cartoon-bookstore-interior-with-books-shelves_107791-20532.jpg?w=1380&t=st=1715273974~exp=1715274574~hmac=6130f8ea83b609c6e25756fc5398b82e2ffa1d0cc8c9e5812c7046f05a8a0d42'); background-repeat:no-repeat; background-size:cover;">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+                <div class="card card-outline card-primary mt-5"  >
+                    <div class="card-header text-center">
+                        <h1>PERPUSTAKAAN SD 01 MINAS TIMUR</h1>
+                    </div>
+                    <div class="card-body">
+                        <form action="{{asset ('AdminLTE/')}}/index3.html" method="post">
+                            <div class="mb-3">
+                                <input type="email" class="form-control" placeholder="Email" required>
+                            </div>
+                            <div class="mb-3">
+                                <input type="password" class="form-control" placeholder="Password" required>
+                            </div>
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-primary btn-lg btn-block">Login</button>
+                            </div>
+                        </form>
+                        <p class="mt-3 mb-0 text-center">
+                            <a href="/Login/Register" class="text-primary">Register a new membership</a>
+                        </p>
+                    </div>
+                </div>
             </div>
-            <div class="card-body">
-                <p class="login-box-msg">Sign in to start your session</p>
-
-                <form action="{{asset ('AdminLTE/')}}/index3.html" method="post">
-                    <div class="input-group mb-3">
-                        <input type="email" class="form-control" placeholder="Email">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="remember">
-                                <label for="remember">
-                                      Remember Me
-                                </label>
-                            </div>
-                        </div>
-                        <!-- /.col -->
-                        <div class="col-4">
-                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                </form>
-                <p class="mb-0">
-                    <a href="/Login/Register" class="text-center">Register a new membership</a>
-                </p>
-            </div>
-            <!-- /.card-body -->
         </div>
-        <!-- /.card -->
     </div>
-    <!-- /.login-box -->
 
-    <!-- jQuery -->
- 
 </body>
 
 </html>
--- resources/views/v_register.blade.php
+++ resources/views/v_register.blade.php
@@ -17,79 +17,49 @@
     <link rel="stylesheet" href="{{asset ('AdminLTE/')}}/dist/css/adminlte.min.css">
 </head>
 
-<body class="hold-transition register-page">
-    <div class="register-box">
-        <div class="register-logo">
-            <a href="{{url('/Login/Register')}}"><b>PERPUSTAKAAN SD 04 MINAS TIMUR</a>
-        </div>
 
-        <div class="card">
-            <div class="card-body register-card-body">
-                <p class="login-box-msg">Register a new membership</p>
-
-                <form action="{{url('/Login/Register')}}" method="post">
-                    @csrf
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control" placeholder="Full name" name="name">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-user"></span>
+<body class="hold-transition login-page" style="background-image: url('https://img.freepik.com/free-vector/cartoon-bookstore-interior-with-books-shelves_107791-20532.jpg?w=1380&t=st=1715273974~exp=1715274574~hmac=6130f8ea83b609c6e25756fc5398b82e2ffa1d0cc8c9e5812c7046f05a8a0d42'); background-repeat:no-repeat; background-size:cover;">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+                <div class="card card-outline card-primary mt-5">
+                    <div class="card-header text-center">
+                        <h1>PERPUSTAKAAN SD 01 MINAS TIMUR</h1>
+                    </div>
+                    <div class="card-body">
+                     <form action="{{url('/Login/Register')}}" method="post">
+                        @csrf
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Full name" name="name">
+                            <div class="input-group-append">
                             </div>
                         </div>
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="email" class="form-control" placeholder="Email" name="email">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
+                        <div class="input-group mb-3">
+                            <input type="email" class="form-control" placeholder="Email" name="email">
+                            <div class="input-group-append">
                             </div>
                         </div>
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Password" name="password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
+                        <div class="input-group mb-3">
+                            <input type="password" class="form-control" placeholder="Password" name="password">
+                            <div class="input-group-append">
                             </div>
                         </div>
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Retype password" name="passwordRetype">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
+                        <div class="input-group mb-3">
+                            <input type="password" class="form-control" placeholder="Retype password" name="passwordRetype">
+                            <div class="input-group-append">
                             </div>
                         </div>
+                                <button type="submit" class="btn btn-primary  btn-block">Register</button>
+                    </form>
+                        <p class="mt-3 mb-0 text-center">
+                            <a href="/Login" class="text-center">I already have a Account</a>
+                        </p>
                     </div>
-                    <div class="row">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="agreeTerms" name="terms" value="agree">
-                                <label for="agreeTerms">
-                                    I agree to the <a href="#">terms</a>
-                                </label>
-                            </div>
-                        </div>
-                        <!-- /.col -->
-                        <div class="col-4">
-                            <button type="submit" class="btn btn-primary btn-block">Register</button>
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                </form>
-                <a href="/Login" class="text-center">I already have a membership</a>
+                </div>
             </div>
-            <!-- /.form-box -->
-        </div><!-- /.card -->
+        </div>
     </div>
-    <!-- /.register-box -->
 
-    <!-- jQuery -->
-    <script src="{{asset ('AdminLTE/')}}/plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="{{asset ('AdminLTE/')}}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="{{asset ('AdminLTE/')}}/dist/js/adminlte.min.js"></script>
 </body>
 
 </html>
