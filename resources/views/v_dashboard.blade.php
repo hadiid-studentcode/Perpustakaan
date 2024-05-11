3fde1ff44d25680d535651512dbea12f8659c787
hadiid-studentcode <hadiidandri2000@gmail.com>
2024-05-11 11:36:21 +0700

    fitur dashboard

--- resources/views/v_dashboard.blade.php
+++ resources/views/v_dashboard.blade.php
@@ -8,13 +8,13 @@
                 <!-- small box -->
                 <div class="small-box bg-info">
                     <div class="inner">
-                        <h3>150</h3>
+                        <h3>{{ $jmlbuku }}</h3>
                         <p>Jumlah Buku</p>
                     </div>
                     <div class="icon">
                         <i class="fas fa-book"></i>
                     </div>
-                    <a href="#" class="small-box-footer">Info Lebih Lanjut <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="{{ url('/Buku') }}" class="small-box-footer">Info Lebih Lanjut <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             <!-- ./col -->
@@ -22,13 +22,13 @@
                 <!-- small box -->
                 <div class="small-box bg-success">
                     <div class="inner">
-                        <h3>53</h3>
+                        <h3>{{ $jmlrak }}</h3>
                         <p>Jumlah Rak Buku</p>
                     </div>
                     <div class="icon">
                         <i class="fas fa-archive"></i>
                     </div>
-                    <a href="#" class="small-box-footer">Info Lebih Lanjut <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="{{ url('/Rak') }}" class="small-box-footer">Info Lebih Lanjut <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             <!-- ./col -->
@@ -36,13 +36,13 @@
                 <!-- small box -->
                 <div class="small-box bg-warning">
                     <div class="inner text-white">
-                        <h3>53</h3>
+                        <h3>{{ $jmlpinjaman }}</h3>
                         <p>Jumlah Pinjaman</p>
                     </div>
                     <div class="icon">
                         <i class="fas fa-handshake"></i>
                     </div>
-                    <a href="#" class="small-box-footer" style="color: white !important;">Info Lebih Lanjut <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="{{ url('/Pinjaman') }}" class="small-box-footer" style="color: white !important;">Info Lebih Lanjut <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
 
@@ -53,13 +53,13 @@
                 <!-- small box -->
                 <div class="small-box bg-danger">
                     <div class="inner">
-                        <h3>65</h3>
+                        <h3>{{ $jmlpengembalian }}</h3>
                         <p>Jumlah Pengembalian</p>
                     </div>
                     <div class="icon">
                         <i class="fas fa-undo"></i>
                     </div>
-                    <a href="#" class="small-box-footer">Info Lebih Lanjut <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="{{ url('/Pengembalian') }}" class="small-box-footer">Info Lebih Lanjut <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             <!-- ./col -->
